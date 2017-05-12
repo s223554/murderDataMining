1473a4e7fd3456649ba16d4640fe5a29a87f7c7e
qian <s223554@gmail.com>
2017-05-11 23:22:44 -0400

    Add more attempts on the data. Split the test set. Main problem, how to use non-number features.

--- Main.py
+++ Main.py
@@ -2,7 +2,11 @@ import numpy as np
 import numpy.random as rnd
 import os
 
+
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+plt.interactive(True)
 
 plt.rcParams['axes.labelsize'] = 14
 plt.rcParams['xtick.labelsize'] = 12
@@ -18,23 +22,23 @@ def save_fig(fig_id, tight_layout=True):
         plt.tight_layout()
     plt.savefig(path, format='png', dpi=300)
 
-# get the data
-DATASETS_URL = "https://www.kaggle.com/murderaccountability/homicide-reports"
 
-import os
-import tarfile
-from six.moves import urllib
+import pandas as pd
 
-HOUSING_PATH = "datasets/murder"
-HOUSING_URL = DATASETS_URL + "/downloads/homicide-reports.zip"
+MURDER_PATH = "datasets/murder"
 
-def fetch_murder_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
-    if not os.path.exists(housing_path):
-        os.makedirs(housing_path)
-    tgz_path = os.path.join(housing_path, "homicide-reports.zip")
-    urllib.request.urlretrieve(housing_url, tgz_path)
-    housing_tgz = tarfile.open(tgz_path)
-    housing_tgz.extractall(path=housing_path)
-    housing_tgz.close()
+def load_data(path=MURDER_PATH):
+    csv_path = os.path.join(path, "database.csv")
+    return pd.read_csv(csv_path)
 
-fetch_murder_data()
+murder_data = load_data()
+
+murder_data['Victim Age'].hist(bins = 300,figsize=(11,8))
+plt.axis([0,100,0,100000])
+
+# split dataset into test.
+
+from sklearn.model_selection import train_test_split
+
+train_set, test_set = train_test_split(murder_data, test_size=0.2, random_state=42)
+test_set.head()
